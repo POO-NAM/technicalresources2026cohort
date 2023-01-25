59723a4356de94ef07cd800ffb7753c0b16e624b
POO-NAM <122135539+POO-NAM@users.noreply.github.com>
2023-01-25 21:18:28 +0530

    submitting question 2 of assignment 1

---
+++ a2_q2.py
@@ -0,0 +1,12 @@
+print("enter the marks")
+m = int(input())
+if m>90:
+    print("excellent")
+elif m>80 and m<90:
+    print("good")
+elif m>70 and m<80:
+    print("fair")
+elif m>60 and m<70:
+    print("meets expectataions")
+else:
+    print("below par")
